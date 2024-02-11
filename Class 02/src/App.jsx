3f62b94562495e8059a8f76b3444fa2249d953ca
Hasnain Khan <itshasnainkhanofficial@gmail.com>
2024-02-11 11:44:18 +0500

    added a folder to explain why use components

---
+++ Class 02/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import WhyComponent from './components/whycomponents/WhyComponent'
+function App() {
+
+  return (
+    <>
+     <WhyComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ Class 02/src/components/whycomponents/WhyComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Mainsection from './Mainsection'
+import Footer from './Footer'
+
+const WhyComponent = () => {
+  return (
+    <div>
+      <Header />
+      <Mainsection />
+      <Mainsection />
+      <Footer />
+    </div>
+  )
+}
+
+export default WhyComponent
