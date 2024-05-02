613922781efe9abb63c672549dd3c0d4a322f1a0
Hasnain Khan <itshasnainkhanofficial@gmail.com>
2024-05-02 11:43:46 +0500

    added final code class 12th

---
+++ class12/backend3/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import {DBConnect} from './config/db.js'
+import { ErrorHandling } from './middleware/ErrorHandling.js'
+import StudentRoutes from './routes/student.js'
+import StaffRoutes from './routes/staff.js'
+
+const app = express()
+dotenv.config();
+DBConnect()
+
+const PORT = process.env.PORT || 3500
+
+
+app.use(express.json()); // Middleware to parse JSON requests
+
+
+
+
+// Student Routes
+app.use('/student', StudentRoutes)
+
+// Staff Routes
+app.use('/staff', StaffRoutes)
+
+// Root route
+app.use('/', (req, res) => res.send("API is running"))
+
+// Route not found
+app.use((req, res, next) => {
+
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  error.status = 404;
+  next(error);
+});
+
+
+// Error handler middleware
+app.use(ErrorHandling);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
+
+
+// Refactor person's CRUD
